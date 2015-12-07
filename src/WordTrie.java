271ee8503d7f5d18a3e3faf45b0edbcb7e1169a7
MadelynTav <Madelyntav@gmail.com>
2015-12-07 17:43:24 -0500

    updates to methods

--- src/WordTrie.java
+++ src/WordTrie.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
+
+import javax.xml.bind.SchemaOutputResolver;
 
 /**
  * Created by c4q-madelyntavarez on 12/5/15.
@@ -10,15 +12,17 @@ import java.util.Stack;
 public class WordTrie {
 
      public class Node {
-         char letter;
+         char   letter;
          Node[] children;
          int index = 0;
+         boolean endOfWord;
 
          public Node(char letter)
          {
              this.letter = letter;
              children = new Node[26];
-             children[0]=this;
+             children[0] = this;
+             endOfWord = false;
              index++;
          }
 
@@ -29,6 +33,13 @@ public class WordTrie {
          public Node[] getChildren()
          {
              return children;
+         }
+
+         public boolean isEndOfWord() { return endOfWord;}
+
+         public void setEndOfWord(boolean endOfWord)
+         {
+             this.endOfWord = endOfWord;
          }
 
          public void addChild(Node node){
@@ -46,43 +57,29 @@ public class WordTrie {
     String word="";
     Node [] rootNodeArray;
     HashMap<Character,Node[]> map;
+    int num=0;
 
     public WordTrie(){
         root=new Node(' ');
         current= root.getChildren();
+        nodes= new Stack<Node>();
         rootNodeArray=root.getChildren();
         map = new HashMap<Character, Node[]>();
         map.put(root.getLetter(),rootNodeArray);
         currentNode=root;
+        words= new ArrayList<String>();
     }
 
 
     public void add(String word) {
         if(word.length()<=0){ return;}
-        System.out.println(word);
-
-        for(Node node: current){
-            if(node==null){
-//                Node newNode= new Node(word.charAt(0));
-//                current=newNode.children;
-//                currentNode=newNode;
-//                add(word.substring(1));
-                continue;
-            }
-
-              if(node.getLetter() == word.charAt(0)){
-                if(word.length()>1)
-                {
-                    current=node.getChildren();
-                    currentNode=node;
-                    add(word.substring(1));
-                } else return;
-            }
-        }
 
         Node node= new Node(word.charAt(0));
         currentNode.addChild(node);
-
+        if(word.length()==1)
+        {
+            node.setEndOfWord(true);
+        }
         if(map.containsKey(word.charAt(0))){
             current=map.get(word.charAt(0));
         } else {
@@ -95,7 +92,6 @@ public class WordTrie {
     }
 
     public boolean contains(String word) {
-        //make a new method that calls everything below
         current=rootNodeArray;
         return getContains(word);
     }
@@ -117,38 +113,37 @@ public class WordTrie {
         return false;
     }
 
-    public List<String> toList() {
+    public List<String> toList()
+    {
         words = new ArrayList<String>();
-        currentNode=root;
-        nodes=new Stack<Node>();
-        nodes.push(currentNode);
-        runToList();
-
+        getAllWords(root);
+        System.out.println(num);
         return words;
     }
 
-    public void runToList(){
+    private void getAllWords(Node node)
+    {
+        if(node==null){
+            return;
+        }
 
-        while(!nodes.isEmpty())
-        {
-            current=map.get( nodes.pop().getLetter());
-
-            for(Node node : current)
+        for(Node child: map.get(node.getLetter())){
+            num++;
+            if(child!=null)
             {
 
-                if(node == null)
+                if(child.getLetter()==' '){
+                    continue;
+                }
+                word += child.getLetter();
+
+                 if(child.endOfWord)
                 {
+                    System.out.println("HERE");
                     words.add(word);
-                    break;
+                    word="";
                 }
-
-                else
-                {
-                   // System.out.println("now");
-                    word += node.getLetter();
-                    nodes.push(node);
-
-                }
+//
             }
         }
     }
@@ -158,13 +153,13 @@ public class WordTrie {
         WordTrie wordTrie= new WordTrie();
         //wordTrie.add("TOMORROW");
         //wordTrie.add("APPLES");
-        //wordTrie.add("COFFEE");
+        //wordTrie.add("COFE");
         wordTrie.add("TRAVEL");
-        wordTrie.add("MADELYN");
+        wordTrie.add("MADELYN");//
         wordTrie.add("COFFEE");
         System.out.println(wordTrie.contains("COFFEE"));
-        System.out.println(wordTrie.contains("MADELYN"));
-        System.out.println(wordTrie.contains("TRAVEL"));
+//        System.out.println(wordTrie.contains("MADELYN"));
+       // System.out.println(wordTrie.contains("TRAVEL"));
         System.out.println(wordTrie.toList());
     }
 
